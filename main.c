41642c988a92cc0faceb11fa9c71cdbd3ac0b92a
BAISSANE <baissanee@gmail.com>
2023-05-28 17:44:49 -0700

    Updates

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
@@ -9,8 +8,10 @@ bus_t bus = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
-	char content[256];
+	char *content;
 	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
@@ -26,11 +27,17 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(content, sizeof(content), file) != NULL)
+	while (read_line > 0)
 	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-		execute(content, &stack, counter, file);
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
 	free_stack(stack);
 	fclose(file);
